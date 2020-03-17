709c611ceb05677c1f9a046ded1f080601189ff9
Sean McDonald <spmcdonald1316@gmail.com>
2020-03-17 15:42:09 -0500

    generate actors controller, add routes and methods for index and show actions

---
+++ app/controllers/api/actors_controller.rb
@@ -0,0 +1,11 @@
+class Api::ActorsController < ApplicationController
+  def index
+    @actors = Actor.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render 'show.json.jb'
+  end
+end
